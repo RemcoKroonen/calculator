27d23f6d70857aa094cceb063a21ef5ccfebcf24
Remco Kroonen <99039459@mydavinci.nl>
2016-12-20 21:19:30 +0100

    bewerking doet het ook en meerdere getallen.

--- calculatorjs.js
+++ calculatorjs.js
@@ -1,3 +1,5 @@
+var somstring = ""
+
 document.getElementById("een").setAttribute("onClick", "javascript:getal(1);");
 document.getElementById("twee").setAttribute("onClick", "javascript:getal(2);");
 document.getElementById("drie").setAttribute("onClick", "javascript:getal(3);");
@@ -8,14 +10,24 @@ document.getElementById("zeven").setAttribute("onClick", "javascript:getal(7);")
 document.getElementById("acht").setAttribute("onClick", "javascript:getal(8);");
 document.getElementById("negen").setAttribute("onClick", "javascript:getal(9);");
 document.getElementById("nul").setAttribute("onClick", "javascript:getal(0);");
-document.getElementById("punt").setAttribute("onClick", "javascript:('.');");
-document.getElementById("is").setAttribute("onClick", "javascript:('=');");
-document.getElementById("keer").setAttribute("onClick", "javascript:('x');");
-document.getElementById("plus").setAttribute("onClick", "javascript:('+');");
-document.getElementById("min").setAttribute("onClick", "javascript:('-');");
-document.getElementById("gedeeld").setAttribute("onClick", "javascript:('/');");
-document.getElementById("c").setAttribute("onClick", "javascript:('c');");
+document.getElementById("punt").setAttribute("onClick", "javascript:bewerking('.');");
+document.getElementById("is").setAttribute("onClick", "javascript:bewerking('=');");
+document.getElementById("keer").setAttribute("onClick", "javascript:bewerking('x');");
+document.getElementById("plus").setAttribute("onClick", "javascript:bewerking('+');");
+document.getElementById("min").setAttribute("onClick", "javascript:bewerking('-');");
+document.getElementById("gedeeld").setAttribute("onClick", "javascript:bewerking('/');");
+document.getElementById("c").setAttribute("onClick", "javascript:reset();");
 
 function getal(cijfer){
-document.getElementById("som").setAttribute("value", cijfer);
+	somstring = somstring+cijfer;
+	document.getElementById("som").setAttribute("value", somstring);
+}
+function bewerking(teken){
+	somstring = somstring+teken;
+	document.getElementById("som").setAttribute("value", somstring);
+}
+function reset(){
+	somstring = "";
+	document.getElementById("som").setAttribute("value", somstring);
+	document.getElementById("resultaat").setAttribute("value", somstring);
 }
