e7602b67b7df283f970ddad98a3da11d2c10fb47
Remco Kroonen <99039459@mydavinci.nl>
2017-01-03 14:46:19 +0100

    kan optellen,delen,keer en min doen.

--- calculatorjs.js
+++ calculatorjs.js
@@ -44,8 +44,52 @@ function reset(){
 	document.getElementById("resultaat").setAttribute("value", somstring);
 }
 function resultaat(){
-	bewerking('');
-	for (somarrayteller =  0; somarrayteller <somarray.length; somarrayteller++ ) {
-		alert(somarray[somarrayteller])
-	}
+    var rekenplek = 0;
+    var uitkomst = 0;
+    bewerking('');
+    /*for (somarrayteller =  0; somarrayteller <somarray.length; somarrayteller++ ) {
+        alert(somarray[somarrayteller])
+    }*/
+    debugger;
+    rekenplek =somarray.findIndex(zoekx);
+    while (rekenplek >= 0){
+        uitkomst = parseFloat(somarray[rekenplek-1]) * parseFloat(somarray[rekenplek+1]);
+        somarray[rekenplek-1] = uitkomst.toString();
+        somarray.splice(rekenplek,2);
+        rekenplek =somarray.findIndex(zoekx);  
+    }
+    rekenplek =somarray.findIndex(zoekgedeeld);
+    while (rekenplek >= 0){
+        uitkomst = parseFloat(somarray[rekenplek-1]) / parseFloat(somarray[rekenplek+1]);
+        somarray[rekenplek-1] = uitkomst.toString();
+        somarray.splice(rekenplek,2);
+        rekenplek =somarray.findIndex(zoekgedeeld);  
+    }
+    rekenplek =somarray.findIndex(zoekplus);
+    while (rekenplek >= 0){
+        uitkomst = parseFloat(somarray[rekenplek-1]) + parseFloat(somarray[rekenplek+1]);
+        somarray[rekenplek-1] = uitkomst.toString();
+        somarray.splice(rekenplek,2);
+        rekenplek =somarray.findIndex(zoekplus);  
+    }
+    rekenplek =somarray.findIndex(zoekmin);
+    while (rekenplek >= 0){
+        uitkomst = parseFloat(somarray[rekenplek-1]) - parseFloat(somarray[rekenplek+1]);
+        somarray[rekenplek-1] = uitkomst.toString();
+        somarray.splice(rekenplek,2);
+        rekenplek =somarray.findIndex(zoekmin);  
+    }
+    document.getElementById("resultaat").setAttribute("value", uitkomst);
+}
+function zoekx(onderdeel){
+    return onderdeel == 'x';
+}
+function zoekgedeeld(onderdeel){
+    return onderdeel == '/';
+}
+function zoekplus(onderdeel){
+    return onderdeel == '+';
+}
+function zoekmin(onderdeel){
+    return onderdeel == '-';
 }
